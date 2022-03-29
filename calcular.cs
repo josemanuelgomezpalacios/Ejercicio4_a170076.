6d966492c450788e4daa62e255a0ce6b975702ec
josemanuelgomezpalacios <101377388+josemanuelgomezpalacios@users.noreply.github.com>
2022-03-29 05:03:41 +0200

    Add files via upload

---
+++ calcular.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Ejercicio_4_a170076
+{
+    public class calcular
+    {
+    }
+}
---
+++ figuraWebService1.asmx
@@ -0,0 +1,1 @@
+﻿<%@ WebService Language="C#" CodeBehind="figuraWebService1.asmx.cs" Class="Ejercicio_4_a170076.figuraWebService1" %>
